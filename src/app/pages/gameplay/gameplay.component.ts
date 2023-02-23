57a0ea3e99ca4a88370bda10a894f036e62ec588
EdyRivas <alexicho03@gmail.com>
2023-02-22 19:00:41 -0700

    android and gameplay section

---
+++ src/app/pages/gameplay/gameplay.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gameplay',
+  templateUrl: './gameplay.component.html',
+  styleUrls: ['./gameplay.component.scss'],
+})
+export class GameplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,12 +5,14 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { StoryComponent } from './story/story.component';
 import { PagesComponent } from './pages.component';
 import { NavBarComponent } from '../layout/nav-bar/nav-bar.component';
+import { GameplayComponent } from './gameplay/gameplay.component';
 
 
 @NgModule({
   declarations: [
     StoryComponent,
     PagesComponent,
+    GameplayComponent,
     NavBarComponent
   ],
   imports: [
