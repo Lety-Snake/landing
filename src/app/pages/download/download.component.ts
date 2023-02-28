c5c7662eba8c864699ba0b461645e4403f792bb0
EdyRivas <alexicho03@gmail.com>
2023-02-27 21:11:41 -0600

    little snake

--- src/app/pages/download/download.component.ts
+++ src/app/pages/download/download.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./download.component.scss'],
 })
 export class DownloadComponent implements OnInit {
+  imgSrc = 'https://th.bing.com/th/id/OIP.QMP71dvZhScaiMLkGDFmDwHaE8?pid=ImgDet&rs=1'
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
