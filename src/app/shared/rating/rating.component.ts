650b01dfe4ea2525672337099301c53a23c35a40
EdyRivas <alexicho03@gmail.com>
2023-03-02 18:54:09 -0600

    voting section and scbar

--- src/app/shared/rating/rating.component.ts
+++ src/app/shared/rating/rating.component.ts
@@ -9,19 +9,35 @@ export class RatingComponent implements OnInit {
   @Input() IdR: string;
   @Input() title: string;
 
+  idRa0: string
   idRa1: string
   idRa2: string
   idRa3: string
   idRa4: string
   idRa5: string
+  emos: string
   constructor() { }
 
   ngOnInit() {
+    this.idRa0 = this.IdR + '-ra0';
     this.idRa1 = this.IdR + '-ra1';
     this.idRa2 = this.IdR + '-ra2';
     this.idRa3 = this.IdR + '-ra3';
     this.idRa4 = this.IdR + '-ra4';
     this.idRa5 = this.IdR + '-ra5';
+    this.emos = this.IdR + '-emos'
   }
-
+  radioCheck(id) {
+    let wrap = document.getElementById(this.emos) as HTMLElement;
+    if (id == this.idRa1)
+      wrap.style.transform = 'translateY(-100px)';
+    else if (id == this.idRa2)
+      wrap.style.transform = 'translateY(-200px)';
+    else if (id == this.idRa3)
+      wrap.style.transform = 'translateY(-300px)';
+    else if (id == this.idRa4)
+      wrap.style.transform = 'translateY(-400px)';
+    else
+      wrap.style.transform = 'translateY(-500px)';
+  }
 }
