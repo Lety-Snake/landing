d10d91282138110608a6065c98edf49f38f0ebfa
EdyRivas <alexicho03@gmail.com>
2023-03-21 17:45:32 -0600

    information changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'lety-landing',
+  appName: 'Letty Snake',
   webDir: 'www',
   bundledWebRuntime: false
 };
